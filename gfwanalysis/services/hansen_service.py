51bc7fbaa261dab61ca2823af341217e21cc6ab3
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-04-20 16:07:01 +0200

    serialisation fixed

--- gfwanalysis/services/hansen_service.py
+++ gfwanalysis/services/hansen_service.py
@@ -5,41 +5,7 @@ import logging
 import ee
 from gfwanalysis.errors import HansenError
 from gfwanalysis.config import SETTINGS
-
-
-def get_region(geom):
-    """Return ee.Geometry from supplied GeoJSON object."""
-    poly = get_coords(geom)
-    ptype = get_type(geom)
-    if ptype.lower() == 'multipolygon':
-        region = ee.Geometry.MultiPolygon(poly)
-    else:
-        region = ee.Geometry.Polygon(poly)
-    return region
-
-
-def get_coords(geojson):
-    if geojson.get('features') is not None:
-        return geojson.get('features')[0].get('geometry').get('coordinates')
-    elif geojson.get('geometry') is not None:
-        return geojson.get('geometry').get('coordinates')
-    else:
-        return geojson.get('coordinates')
-
-
-def get_type(geojson):
-    if geojson.get('features') is not None:
-        return geojson.get('features')[0].get('geometry').get('type')
-    elif geojson.get('geometry') is not None:
-        return geojson.get('geometry').get('type')
-    else:
-        return geojson.get('type')
-
-
-def squaremeters_to_ha(value):
-    """Converts square meters to hectares, and gives val to 2 decimal places"""
-    tmp = value/10000.
-    return float('{0:4.2f}'.format(tmp))
+from gfwanalysis.utils.spatial import get_region, squaremeters_to_ha
 
 
 class HansenService(object):
