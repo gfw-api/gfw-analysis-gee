85d247f763fcfdfacbe7f27398743c215d3b8526
Benjamin A. Laken <benlaken@gmail.com>
2017-04-21 12:43:39 +0200

    modified count function inline with Brookies comments

--- gfwanalysis/services/forma250_service.py
+++ gfwanalysis/services/forma250_service.py
@@ -26,13 +26,10 @@ class Forma250Service(object):
         a simple count of pixels where clearing occured over a date range.
         """
         try:
-            max_pixels = 9999999999
-            scale = 231.65635826395828
             region = get_region(geojson)
             asset_id = SETTINGS.get('gee').get('assets').get('forma250GFW')
             logging.info(asset_id)
-            ic=ee.ImageCollection(asset_id
-                                  ).sort('system:time_start', False)
+            ic=ee.ImageCollection(asset_id).sort('system:time_start', False)
             latest = ee.Image(ic.first())
             alert_date_band = latest.select('alert_date')
             milisec_date_start = ee.Date(start_date).millis()
@@ -42,24 +39,16 @@ class Forma250Service(object):
             reduce_sum_args = {'reducer': ee.Reducer.sum().unweighted(),
                                'geometry': region,
                                'bestEffort': True,
-                               'scale': scale,
+                               'scale': 231.65635826395828,
                                'crs': "EPSG:4326",
-                               'maxPixels': max_pixels}
-            reduce_count_args = {'reducer': ee.Reducer.count().unweighted(),
-                                 'geometry': region,
-                                 'bestEffort': True,
-                                 'scale': scale,
-                                 'crs': "EPSG:4326",
-                                 'maxPixels': max_pixels}
+                               'maxPixels': 9999999999}
             area_m2 = latest.select('alert_delta').mask(date_mask).divide(100).multiply(
                         ee.Image.pixelArea()).reduceRegion(**reduce_sum_args).getInfo()
-            # Below is where the E.E. requests are made
             alert_area_ha = squaremeters_to_ha(area_m2['alert_delta'])
-            tmp_counts = latest.select('alert_delta').mask(
-                            date_mask).reduceRegion(**reduce_count_args).getInfo()
-            alert_counts = tmp_counts['alert_delta']
-            logging.info(f"Number of alerts over time period = {alert_counts}")
-            logging.info(f"Estimated area loss over time period = {alert_area_ha} ha")
+            tmp_counts = date_mask.gt(0).reduceRegion(**reduce_sum_args).getInfo()
+            alert_counts = int(tmp_counts['alert_delta'])
+            #logging.info(f"Number of alerts over time period = {alert_counts}")
+            #logging.info(f"Estimated area loss over time period = {alert_area_ha} ha")
             return {'area_ha':alert_area_ha, 'alert_counts':alert_counts}
         except Exception as error:
             logging.error(str(error))
