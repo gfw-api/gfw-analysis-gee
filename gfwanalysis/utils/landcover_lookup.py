2a1cc187d5eeb7b9fee5d811275b32ebef9541fd
mappingvermont <charlie.hofmann@gmail.com>
2017-11-06 15:48:16 -0500

    add SEA and IDN landcover datasets to landcover and loss-by-landcover analyses

--- gfwanalysis/utils/landcover_lookup.py
+++ gfwanalysis/utils/landcover_lookup.py
@@ -128,7 +128,33 @@ all_lulc_dict = {
 
     'mangroves': {
         '1': 'Landsat Mangroves 2000'
-    }
+    },
 
+    'idn-landcover': {
+        '0': 'Secondary forest',
+        '1': 'Primary forest',
+        '2': 'Timber plantation',
+        '3': 'Agriculture',
+        '4': 'Settlement',
+        '5': 'Swamp',
+        '6': 'Grassland/shrub',
+        '7': 'Bare land',
+        '8': 'Estate crop plantation',
+        '9': 'Body of water',
+        '10': 'Fish pond',
+        '11': 'Mining'
+    },
+
+    'sea-landcover': {
+        '0': 'Mining',
+        '1': 'Mixed tree crops',
+        '2': 'No data',
+        '3': 'Oil palm plantation',
+        '4': 'Settlements',
+        '5': 'Swamp',
+        '6': 'Timber plantation',
+        '7': 'Primary forest',
+        '8': 'Water bodies'
+    }
 
 }
