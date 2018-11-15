4426dd907613af5af1ef37497db0abab358ee9bb
AJ Pain <aj.pain89@gmail.com>
2018-11-15 14:03:43 -0500

    Initial commit (#50)

--- gfwanalysis/routes/api/v1/whrc_biomass_router.py
+++ gfwanalysis/routes/api/v1/whrc_biomass_router.py
@@ -15,12 +15,12 @@ from gfwanalysis.middleware import get_geo_by_hash, get_geo_by_use, get_geo_by_w
 from gfwanalysis.errors import WHRCBiomassError
 from gfwanalysis.serializers import serialize_whrc_biomass
 
-whrc_biomass_endpoints_v1 = Blueprint('whrc_biomass', __name__)
+whrc_biomass_endpoints_v1 = Blueprint('whrc_biomass_endpoints_v1', __name__)
 
 
 def analyze(geojson, area_ha):
     """Analyze WHRC Biomass"""
-    logging.info('[ROUTER]: Getting biomassloss')
+    logging.info('[ROUTER]: Getting biomass')
     if not geojson:
         return error(status=400, detail='Geojson is required')
 
@@ -32,7 +32,7 @@ def analyze(geojson, area_ha):
             threshold=threshold,
             begin=begin,
             end=end)
-    except BiomassLossError as e:
+    except WHRCBiomassError as e:
         logging.error('[ROUTER]: '+e.message)
         return error(status=500, detail=e.message)
     except Exception as e:
@@ -48,7 +48,7 @@ def analyze(geojson, area_ha):
 @get_geo_by_hash
 def get_by_geostore(geojson, area_ha):
     """By Geostore Endpoint"""
-    logging.info('[ROUTER]: Getting biomassloss by geostore')
+    logging.info('[ROUTER]: Getting biomass by geostore')
     return analyze(geojson, area_ha)
 
 
@@ -56,7 +56,7 @@ def get_by_geostore(geojson, area_ha):
 @get_geo_by_use
 def get_by_use(name, id, geojson, area_ha):
     """Use Endpoint"""
-    logging.info('[ROUTER]: Getting biomassloss by use')
+    logging.info('[ROUTER]: Getting biomass by use')
     return analyze(geojson, area_ha)
 
 
@@ -64,7 +64,7 @@ def get_by_use(name, id, geojson, area_ha):
 @get_geo_by_wdpa
 def get_by_wdpa(id, geojson, area_ha):
     """Wdpa Endpoint"""
-    logging.info('[ROUTER]: Getting biomassloss by wdpa')
+    logging.info('[ROUTER]: Getting biomass by wdpa')
     return analyze(geojson, area_ha)
 
 
@@ -72,7 +72,7 @@ def get_by_wdpa(id, geojson, area_ha):
 @get_geo_by_national
 def get_by_national(iso, geojson, area_ha):
     """National Endpoint"""
-    logging.info('[ROUTER]: Getting biomassloss by national')
+    logging.info('[ROUTER]: Getting biomass by national')
     return analyze(geojson, area_ha)
 
 
@@ -80,12 +80,12 @@ def get_by_national(iso, geojson, area_ha):
 @get_geo_by_subnational
 def get_by_subnational(iso, id1, geojson, area_ha):
     """Subnational Endpoint"""
-    logging.info('[ROUTER]: Getting biomassloss by subnational')
+    logging.info('[ROUTER]: Getting biomass by subnational')
     return analyze(geojson, area_ha)
 
 @whrc_biomass_endpoints_v1.route('/admin/<iso>/<id1>/<id2>', strict_slashes=False, methods=['GET'])
 @get_geo_by_regional
 def get_geo_by_regional(iso, id1, id2, geojson, area_ha):
     """Subnational Endpoint"""
-    logging.info('[ROUTER]: Getting biomassloss by subnational')
+    logging.info('[ROUTER]: Getting biomass by subnational')
     return analyze(geojson, area_ha)
