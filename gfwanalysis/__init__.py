e0c9ded7f926288dd453f9339d163c68a18ca687
Adam J. Pain <aj.pain89@gmail.com>
2018-11-15 13:09:06 -0500

    Various updates

--- gfwanalysis/__init__.py
+++ gfwanalysis/__init__.py
@@ -17,7 +17,7 @@ from gfwanalysis.routes.api import error
 from gfwanalysis.routes.api.v1 import hansen_endpoints_v1, forma250_endpoints_v1, \
     biomass_loss_endpoints_v1, landsat_tiles_endpoints_v1, histogram_endpoints_v1, \
     landcover_endpoints_v1, sentinel_tiles_endpoints_v1, highres_tiles_endpoints_v1, \
-    recent_tiles_endpoints_v1
+    recent_tiles_endpoints_v1, whrc_biomass_endpoints_v1
 from gfwanalysis.utils.files import load_config_json
 import CTRegisterMicroserviceFlask
 
@@ -52,6 +52,7 @@ app.register_blueprint(highres_tiles_endpoints_v1, url_prefix='/api/v1/highres-t
 app.register_blueprint(recent_tiles_endpoints_v1, url_prefix='/api/v1/recent-tiles')
 app.register_blueprint(histogram_endpoints_v1, url_prefix='/api/v1/loss-by-landcover')
 app.register_blueprint(landcover_endpoints_v1, url_prefix='/api/v1/landcover')
+app.register_blueprint(whrc_biomass_endpoints_v1, url_prefix='/api/v1/whrc-biomass')
 
 # CT
 info = load_config_json('register')
