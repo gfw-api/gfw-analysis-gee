01439e4c8fcbf6ec06023cfc2eec328783a0ac48
Benjamin A. Laken <benlaken@gmail.com>
2019-02-08 16:11:49 +0100

    added admin1 dynamic simplification

--- gfwanalysis/services/geostore_service.py
+++ gfwanalysis/services/geostore_service.py
@@ -2,7 +2,7 @@
 
 from gfwanalysis.errors import GeostoreNotFound
 from CTRegisterMicroserviceFlask import request_to_microservice
-from gfwanalysis.utils.geo import admin_0_simplify
+from gfwanalysis.utils.geo import admin_0_simplify, admin_1_simplify
 import logging
 
 class GeostoreService(object):
@@ -65,9 +65,15 @@ class GeostoreService(object):
 
     @staticmethod
     def get_subnational(iso, id1):
-        # lookup table here too
+        #logging.info('[geostore service]: in get_subnational function')
+        simplification = admin_1_simplify(iso, id1)
+        #logging.info(f'[geostore service]: simplification={simplification}')
+        if simplification:
+            url = f'/v2/geostore/admin/{iso}/{id1}?simplify={simplification}'
+        else:
+            url = f'/v2/geostore/admin/{iso}/{id1}'
         config = {
-            'uri': '/v2/geostore/admin/'+iso+'/'+id1,
+            'uri': url,
             'method': 'GET',
             'ignore_version': True
         }
