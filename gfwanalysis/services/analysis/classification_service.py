06b41625aa4122b97887bf05789f27291470ff0b
Nico <nrigheriu@gmail.com>
2019-03-21 15:35:50 +0100

    landsat now works too

--- gfwanalysis/services/analysis/classification_service.py
+++ gfwanalysis/services/analysis/classification_service.py
@@ -15,24 +15,22 @@ class ClassificationService(object):
         For a given area classify the forest amount and return a image with the classified forest and amount.
             example of a valid img_id = 'COPERNICUS/S2/20181222T115211_20181222T115210_T28RCS'
         """
-        logging.info(f'[classification_service]: img_id={img_id}')
         try:
+            instrument = ""     #figure out if instrument is sentinel or landsat
+            if (img_id.startswith("COPERNICUS")):
+                instrument = "sentinel"
+            elif (img_id.startswith("LANDSAT")):
+                instrument = "landsat"
             # If one exists restore it from local storage
-            model = get_model_from_local() 
+            model = create_model(instrument)
             if(model == None):
-                # If none exists in local storage restore from google storage
-                logging.info(f"[classification_service]: model not found locally!")
-                model = get_model_from_bucket()
-                if(model == None):
-                    # If none exists in google storage create one (and add it to local and remote storage)
-                    #model = create_model()
-                    pass
-
+                # If there's an error when creating, get default one from local storage
+                model = get_model_from_local() 
             # grab the image specified by the ID
             image = get_image(img_id)
             # Apply classifer to specified L8 or S2 image
             #logging.info(f"[classification_service]: classified version = {classified_image}")
-            classified_image = classify_image(image, model)
+            classified_image = classify_image(image, model, instrument)
             # Generate tile url and return in d['url'] object
             url = get_image_url(classified_image)
             logging.info(f'[classification_service]: passed main logic url={url}')
@@ -53,41 +51,39 @@ def get_image_url(classified_image):
     url = (base_url + '/map/' + d['mapid'] + '/{z}/{x}/{y}?token=' + d['token'])
     return url
 
-def classify_image(image, model):
-    return image.select("B4","B3","B2").divide(100*100).classify(model)
-
-def create_model():
-    logging.info(f'[classification_service]: attempting to create model')
+def classify_image(image, model, instrument):
+    if (instrument == "sentinel"):
+        return image.select("B4","B3","B2").divide(100*100).classify(model)
+    if (instrument == "landsat"):
+        return image.select("B4","B3","B2").classify(model)
+def create_model(instrument):
     try:
-        # find a way of checking if model is in local memory (probably using OS?)
-        return ""
+        simple_bands = ['B2', 'B3', 'B4']
+        labeled_bands = simple_bands
+        labeled_bands.append('cropland')
+        if (instrument == 'sentinel'):
+            trainingPoints = ee.FeatureCollection('projects/wri-datalab/trainingPoints')
+        elif (instrument == 'landsat'):
+            trainingPoints = ee.FeatureCollection('projects/wri-datalab/trainingLandsatPoints')
+        classifier_args = {'features': trainingPoints, 'classProperty':'cropland',\
+                                    'inputProperties':labeled_bands}
+        classifier = ee.Classifier.randomForest(15).train(**classifier_args)
+        return classifier
     except:
         return None
 
 def get_image(img_id):
     """Check if S2 or L8 image and treat image accordingly"""
-    logging.info(f'[classification_service]: attempting to get image {img_id}')
     try:
         result_img = ee.Image(img_id)
-        #logging.info(f'[classification_service]: attempting to get image {result_img.getInfo()}')
         return result_img
     except:
         return None
 
 def get_model_from_local():
     try:
-        # find a way of checking if model is in local memory (probably using OS?)
-        # use OS to test if model is saved as a local file
-        simple_bands = ['B2', 'B3', 'B4']
-        labeled_bands = simple_bands
-        labeled_bands.append('cropland')
-        trainingPoints = ee.FeatureCollection('projects/wri-datalab/trainingPoints')
-        classifier_args = {'features': trainingPoints, 'classProperty':'cropland',\
-                                     'inputProperties':labeled_bands}
-        classifier = ee.Classifier.randomForest(15).train(**classifier_args)
-        return classifier
-        #model = pickle.load(open("/opt/gfwanalysis/classifier.pkl", 'rb'))      
-        #return model
+        model = pickle.load(open("/opt/gfwanalysis/classifier.pkl", 'rb'))      
+        return model
     except:
         return None
 
