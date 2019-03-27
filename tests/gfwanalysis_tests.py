85fe31c483951aae7d5eaa8b8ff568a5224d033d
Benjamin A. Laken <benlaken@gmail.com>
2019-03-27 09:39:00 +0100

    added simple test that should pass

--- tests/gfwanalysis_tests.py
+++ tests/gfwanalysis_tests.py
@@ -1,2 +1,13 @@
-def test_first():
-    assert True == True
+import gfwanalysis
+
+def test_serialize_umd():
+    d = {'area_ha': 0.0,
+         'tree_extent': 0,
+         'tree_extent2010': 0,
+         'gain': 0,
+         'loss': 0
+        }
+    id_type = 'umd'
+    s_obj = gfwanalysis.serializers.serialize_umd(d, id_type)
+    assert s_obj.get('type') == id_type
+    assert s_obj.get('attributes').get('loss') is not None
