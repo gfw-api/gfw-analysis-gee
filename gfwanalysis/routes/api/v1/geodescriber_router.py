012c506c4dcf0a3fee001ac18f158265ca73420e
Benjamin A. Laken <benlaken@gmail.com>
2019-05-27 12:45:58 +0200

    added a geom route to geodescriber

--- gfwanalysis/routes/api/v1/geodescriber_router.py
+++ gfwanalysis/routes/api/v1/geodescriber_router.py
@@ -9,7 +9,7 @@ import logging
 from flask import jsonify, request, Blueprint
 from gfwanalysis.routes.api import error, set_params
 from gfwanalysis.validators import validate_geostore
-from gfwanalysis.middleware import get_geo_by_hash
+from gfwanalysis.middleware import get_geo_by_hash, get_geo_by_geom
 from gfwanalysis.services.analysis.geodescriber import GeodescriberService
 from gfwanalysis.errors import GeodescriberError
 
@@ -21,8 +21,8 @@ def analyze(geojson, area_ha):
     app = request.args.get('app', 'gfw')
     lang = request.args.get('lang','en')
     #logging.info(f'[ROUTER]: Geodescriber params {geojson}, {app}, {lang}')
-    gd_result = GeodescriberService.analyze(geojson=geojson, area_ha=area_ha,
-                                             app=app, lang=lang)
+    gd_result = GeodescriberService.analyze(geojson=geojson,
+                                            area_ha=area_ha, app=app, lang=lang)
     #logging.info(f'[ROUTER]: result {gd_result}')
     return jsonify(data=gd_result), 200
 
@@ -33,4 +33,12 @@ def analyze(geojson, area_ha):
 def get_by_geostore(geojson, area_ha):
     """By Geostore Endpoint"""
     logging.info('[ROUTER - geodescriber]: Getting area by geostore')
+    return analyze(geojson, area_ha)
+
+
+@geodescriber_endpoints_v1.route('/geom/', strict_slashes=False, methods=['GET','POST'])
+@get_geo_by_geom
+def get_by_geom(geojson, area_ha):
+    """By Geostore Endpoint"""
+    logging.info('[ROUTER - geodescriber]: Getting area by geom')
     return analyze(geojson, area_ha)
