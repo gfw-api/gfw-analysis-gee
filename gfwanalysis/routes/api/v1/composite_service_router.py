11bb923718618952fd2a3f859fddb9d78dbb66a6
Benjamin A. Laken <benlaken@gmail.com>
2019-06-03 15:44:47 +0200

    small partial bugfix

--- gfwanalysis/routes/api/v1/composite_service_router.py
+++ gfwanalysis/routes/api/v1/composite_service_router.py
@@ -21,8 +21,8 @@ def composite(geojson, instrument, date_range, thumb_size, classify, band_viz, g
     Get a composite satellite image for a geostore id .
     """
     try:
-        data = CompositeService.get_composite_image(geojson=geojson, instrument=instrument\
-            , date_range=date_range, thumb_size=thumb_size, classify=classify,\
+        data = CompositeService.get_composite_image(geojson=geojson, instrument=instrument,\
+                 date_range=date_range, thumb_size=thumb_size, classify=classify,\
                  band_viz=band_viz, get_stats=get_stats, get_dem=get_dem)
     except CompositeError as e:
         logging.error(f'[ROUTER]: {e.message}')
