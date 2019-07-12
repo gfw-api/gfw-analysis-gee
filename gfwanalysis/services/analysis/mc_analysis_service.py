e7c9934cef59ef83fe6e6e7995768b942b06417c
Benjamin A. Laken <benlaken@gmail.com>
2019-07-12 14:06:52 +0200

    work in progress on dockerfile change to slim

--- gfwanalysis/services/analysis/mc_analysis_service.py
+++ gfwanalysis/services/analysis/mc_analysis_service.py
@@ -2,16 +2,24 @@
 
 import ee
 import logging
+import pandas as pd
 from gfwanalysis.errors import MCAnalysisError
 
 
 class MCAnalysisService(object):
 
     @staticmethod
-    def analyze(timeseries, window=5, bin_number=100, mc_number=1000):
+    def analyze(timeseries, window, bin_number, mc_number):
         """This is the Monte Carlo Analysis Service
         """
-        logging.info(f"[MCService] {timeseries}, {window}, {bin_number}, {mc_number}")
+        if not window:
+            window = 5
+        if not bin_number:
+            bin_number = 100
+        if not mc_number:
+            mc_number = 1000
+        logging.info(f"[MC Service] {timeseries}, {window}, {bin_number}, {mc_number}")
+        logging.info(f"[MC service] pandas: {pd.__version__}")
         d={}
         d["window"]=window
         d["bin_number"]=bin_number
