73e0e8c40db1a19716e95ba3b399451228e6e852
Benjamin A. Laken <benlaken@gmail.com>
2019-07-15 11:56:28 +0200

    elenas first update attempt

--- gfwanalysis/services/analysis/mc_analysis_service.py
+++ gfwanalysis/services/analysis/mc_analysis_service.py
@@ -3,6 +3,7 @@
 import ee
 import logging
 import pandas as pd
+import numpy as np
 from gfwanalysis.errors import MCAnalysisError
 
 
@@ -24,4 +25,24 @@ class MCAnalysisService(object):
         d["window"]=window
         d["bin_number"]=bin_number
         d["mc_number"]=mc_number
+        df = pd.DataFrame(list(timeseries.values()), index= list(timeseries.keys()), columns = ["Carbon_emissions"])
+        boxcar = df.rolling(window=window, min_periods=window,win_type="boxcar", center=True).mean()
+        t0_sigma=np.std(df.values[0:window])
+        logging.info(f"[MC service] pandas: {t0_sigma}")
+        tn_sigma=np.std(df.values[-window:])
+        logging.info(f"[MC service] pandas: {tn_sigma}")
+        cumulative_sigma = np.sqrt(t0_sigma**2 + tn_sigma**2)
+        logging.info(f"[MC service] pandas: {cumulative_sigma}")
+        logging.info(f"[MC service] pandas: {cumulative_sigma}")
+        boxcar_values = boxcar.carbon_emissions.values
+        mask = np.isnan(boxcar_values)
+        cleaned_boxcar_values = boxcar_values[mask != True]
+        t0 = cleaned_boxcar_values[0]
+        logging.info(f"[MC service] pandas: {t0}")
+        tn = cleaned_boxcar_values[-1]
+        logging.info(f"[MC service] pandas: {tn}")
+        anomaly = tn - t0
+        logging.info(f"[MC service] pandas: {anomaly}")
+
+
         return d
