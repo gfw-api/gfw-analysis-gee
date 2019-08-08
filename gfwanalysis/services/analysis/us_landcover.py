c19a3234d8440927ffbfd945595cf7af1480ac52
Elena Palao Martinez <elena.palao@vizzuality.com>
2019-08-08 17:40:42 +0200

    updats everything

--- gfwanalysis/services/analysis/us_landcover.py
+++ gfwanalysis/services/analysis/us_landcover.py
@@ -10,74 +10,54 @@ import logging
 from gfwanalysis.config import SETTINGS
 from gfwanalysis.errors import NLCDLandcoverError
 from gfwanalysis.utils.geo import get_region
-
+from gfwanalysis.utils.image_col_reducer import ImageColIntersect
 
 class NLCDLandcoverService(object):
-
     @staticmethod
     def analyze(geojson):
         """
-        Takes WHRC Carbon and Hansen loss data and returns amount of biomass, carbon and co2 lost
-        by time.
+        
         """
         try:
-            logging.info(f'[nlcd-landcover-service]: with properties passed: {threshold}, {begin}, {end}')
-            # hansen tree cover loss by year
+            logging.info(f'[nlcd-landcover-service]: Initialize analysis')
+            # nlcd land cover
             d = {}
             # Extract int years to work with for time range
-            start_year = int(begin.split('-')[0]) - 2000
-            end_year = int(end.split('-')[0]) - 2000
+            valid_years = [
+                {'id':'NLCD2001', 'year': 2001},
+                {'id':'NLCD2006', 'year': 2006},
+                {'id':'NLCD2011', 'year': 2011},
+                {'id':'NLCD2016', 'year': 2016}
+          ]
 
             # Gather assets
-            band_name = f'loss_{threshold}'
-            hansen_asset = SETTINGS.get('gee').get('assets').get('hansen')
-            biomass_asset = SETTINGS.get('gee').get('assets').get('whrc_biomass')
-            hansen = ee.Image(hansen_asset).select(band_name)
-            biomass = ee.ImageCollection(biomass_asset).max()
-            region = get_region(geojson)
-            logging.info(f'[nlcd-landcover-service]: built assets for analysis, using Hansen band {band_name}')
+            band_name = 'landcover'
+            landcover_asset = SETTINGS.get('gee').get('assets').get('us_landcover')
+            image_list = [ee.Image(f"{landcover_asset}/{year.get('id')}") for year in valid_years]
+            us_landcover = ee.ImageCollection(image_list).select(band_name)
+            region = ee.Geometry(geojson)
+            scale = 30
+            logging.info(f'[nlcd-landcover-service]: built assets for analysis, using {band_name}')
 
-            # Reducer arguments
-            reduce_args = {
-                'reducer': ee.Reducer.sum().unweighted(),
-                'geometry': region,
-                'bestEffort': True,
-                'scale': 30,
-                'tileScale': 16
-            }
 
-            # mask hasen data with itself (important step to prevent over-counting)
-            hansen = hansen.mask(hansen)
+            # Calculate landcover with a collection operation method
+            stats = us_landcover.map(ImageColIntersect(region, scale))
 
-            # Calculate annual biomass loss with a collection operation method
-            def reduceFunction(img):
-                out = img.reduceRegion(**reduce_args)
-                return ee.Feature(None, img.toDictionary().combine(out))
+            # Format data structure
 
-            # Calculate annual biomass loss - add subset images to a collection and then map a reducer to it
-            # Calculate stats 10000 ha, 10^6 to transform from Mg (10^6g) to Tg(10^12g) and 255 as is the pixel value
-            collectionG = ee.ImageCollection([biomass.multiply(ee.Image.pixelArea().divide(10000)).mask(
-                hansen.updateMask(hansen.eq(year))).set({'year': 2000 + year})
-                                              for year in range(start_year, end_year + 1)])
-            output = collectionG.map(reduceFunction).getInfo()
+            data = [{
+                'id': d['id'],
+                'stats': { k: v * 30 * 30 * 1e-4 for k,v in d['properties']['stats'].items() }
+                }
+                for d in stats['features']]
 
-            # # Convert to carbon and co2 values and add to output dictionary
-            biomass_to_carbon = 0.5
-            carbon_to_co2 = 3.67
+            for el in data:
+                year_id = el['id']
+                year = [y['year'] for y in valid_years if y['id']== year_id][0] 
+                d[year] = el['stats']
 
-            d['biomassLossByYear'] = {}
-            d['cLossByYear'] = {}
-            d['co2LossByYear'] = {}
-            for row in output.get('features'):
-                yr = row.get('properties').get('year')
-                d['biomassLossByYear'][yr] = row.get('properties').get('b1')
-                d['cLossByYear'][yr] = float(f"{d.get('biomassLossByYear').get(yr) * biomass_to_carbon :3.2f}")
-                d['co2LossByYear'][yr] = float(f"{d.get('cLossByYear').get(yr) * carbon_to_co2 :3.2f}")
-
-            # # Calculate total biomass loss
-            d['biomassLoss'] = sum([d.get('biomassLossByYear').get(y) for y in d.get('biomassLossByYear')])
             return d
 
         except Exception as error:
             logging.error(str(error))
-            raise BiomassLossError(message='Error in BiomassLoss Analysis')
+            raise NLCDLandcover(message='Error in LandCover Analysis')
---
+++ gfwanalysis/utils/image_col_reducer.py
@@ -0,0 +1,13 @@
+def ImageColIntersect(geom, scale, reducer_type):
+    def reducerWrapper(image):
+        stats = image.reduceRegion(
+            reducer=reducer_type,
+            geometry=geom, 
+            scale=scale, 
+            maxPixels=1e18,
+            bestEffort=False, 
+            crs='EPSG:4326'
+        )
+        feat = ee.Feature(None).set(stats)
+        return feat
+    return reducerWrapper
