365de48bf1c3be42b60272212af5d8ffbf5c484e
Elena Palao Martinez <elena.palao@vizzuality.com>
2019-08-09 11:43:09 +0200

    add routers

--- gfwanalysis/routes/api/v2/nlcd_landcover_router.py
+++ gfwanalysis/routes/api/v2/nlcd_landcover_router.py
@@ -48,42 +48,42 @@ def get_by_geostore(geojson, area_ha):
     return analyze(geojson, area_ha)
 
 
-# @biomass_loss_endpoints_v2.route('/use/<name>/<id>', strict_slashes=False, methods=['GET'])
-# @get_geo_by_use
-# def get_by_use(name, id, geojson, area_ha):
-#     """Use Endpoint"""
-#     logging.info('[ROUTER]: Getting biomassloss by use')
-#     return analyze(geojson, area_ha)
+@nlcd_landcover_endpoints_v2.route('/use/<name>/<id>', strict_slashes=False, methods=['GET'])
+@get_geo_by_use
+def get_by_use(name, id, geojson, area_ha):
+    """Use Endpoint"""
+    logging.info('[ROUTER]: Getting nlcd landcover by use')
+    return analyze(geojson, area_ha)
 
 
-# @biomass_loss_endpoints_v2.route('/wdpa/<id>', strict_slashes=False, methods=['GET'])
-# @get_geo_by_wdpa
-# def get_by_wdpa(id, geojson, area_ha):
-#     """Wdpa Endpoint"""
-#     logging.info('[ROUTER]: Getting biomassloss by wdpa')
-#     return analyze(geojson, area_ha)
+@nlcd_landcover_endpoints_v2.route('/wdpa/<id>', strict_slashes=False, methods=['GET'])
+@get_geo_by_wdpa
+def get_by_wdpa(id, geojson, area_ha):
+    """Wdpa Endpoint"""
+    logging.info('[ROUTER]: Getting nlcd landcover by wdpa')
+    return analyze(geojson, area_ha)
 
 
-# @biomass_loss_endpoints_v2.route('/admin/<iso>', strict_slashes=False, methods=['GET'])
-# @get_geo_by_national
-# def get_by_national(iso, geojson, area_ha):
-#     """National Endpoint"""
-#     logging.info('[ROUTER]: Getting biomassloss by iso')
-#     return analyze(geojson, area_ha)
+@nlcd_landcover_endpoints_v2.route('/admin/<iso>', strict_slashes=False, methods=['GET'])
+@get_geo_by_national
+def get_by_national(iso, geojson, area_ha):
+    """National Endpoint"""
+    logging.info('[ROUTER]: Getting nlcd landcover by iso')
+    return analyze(geojson, area_ha)
 
 
-# @biomass_loss_endpoints_v2.route('/admin/<iso>/<id1>', strict_slashes=False, methods=['GET'])
-# @get_geo_by_subnational
-# def get_by_subnational(iso, id1, geojson, area_ha):
-#     """Subnational Endpoint"""
-#     logging.info('[ROUTER]: Getting biomassloss by admin1')
-#     logging.info(f'[ROUTER]: admin1 area_ha = {area_ha}')
-#     return analyze(geojson, area_ha)
+@nlcd_landcover_endpoints_v2.route('/admin/<iso>/<id1>', strict_slashes=False, methods=['GET'])
+@get_geo_by_subnational
+def get_by_subnational(iso, id1, geojson, area_ha):
+    """Subnational Endpoint"""
+    logging.info('[ROUTER]: Getting nlcd landcover by admin1')
+    logging.info(f'[ROUTER]: admin1 area_ha = {area_ha}')
+    return analyze(geojson, area_ha)
 
 
-# @biomass_loss_endpoints_v2.route('/admin/<iso>/<id1>/<id2>', strict_slashes=False, methods=['GET'])
-# @get_geo_by_regional
-# def get_by_regional(iso, id1, id2, geojson, area_ha):
-#     """Subnational Endpoint"""
-#     logging.info('[ROUTER]: Getting biomassloss by admin2')
-#     return analyze(geojson, area_ha)
+@nlcd_landcover_endpoints_v2.route('/admin/<iso>/<id1>/<id2>', strict_slashes=False, methods=['GET'])
+@get_geo_by_regional
+def get_by_regional(iso, id1, id2, geojson, area_ha):
+    """Subnational Endpoint"""
+    logging.info('[ROUTER]: Getting nlcd landcover by admin2')
+    return analyze(geojson, area_ha)
