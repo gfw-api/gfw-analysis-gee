33435b0deeda7b9e8db99fbef8934b709843346a
Adam J. Pain <aj.pain89@gmail.com>
2019-08-09 11:33:17 +0200

    Change router name

--- gfwanalysis/routes/api/v2/__init__.py
+++ gfwanalysis/routes/api/v2/__init__.py
@@ -1,3 +1,3 @@
 from gfwanalysis.routes.api.v2.biomass_loss_router import biomass_loss_endpoints_v2
 from gfwanalysis.routes.api.v2.landsat_router import landsat_tiles_endpoints_v2
-from gfwanalysis.routes.api.v2.us_landcover_router import nlcd_landcover_endpoints_v2
+from gfwanalysis.routes.api.v2.nlcd_landcover_router import nlcd_landcover_endpoints_v2
