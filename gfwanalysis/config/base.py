8e6d60255cc7f91980787ab7c4af07f5a6c6e1ab
Elpamar <49622839+elpamart@users.noreply.github.com>
2019-08-09 12:25:33 +0200

    Feature/nldc landcover (#138)
    * Develop (#136)
    
    * fix title key
    
    * Add additional title logic
    
    * Develop (#137)
    
    * fix title key
    
    * Add additional title logic
    
    * Fix full stop at the end of description
    
    * Fix description
    
    * initial commit
    
    * add router and part of the service
    
    * updats everything
    
    *  fix ruting
    
    * fix the service
    
    * add land use class name
    
    * Change router name
    
    * add routers
    
    * add routers
    
    * resolve changes

--- gfwanalysis/config/base.py
+++ gfwanalysis/config/base.py
@@ -34,6 +34,7 @@ SETTINGS = {
             'sea-landcover': 'projects/wri-datalab/gfw-api/sea-landcover',
             'forma250GFW': 'projects/wri-datalab/FORMA250',
             'whrc_biomass': 'projects/wri-datalab/WHRC_CARBON',
+            'us_landcover': 'USGS/NLCD',
             'biomassloss_v1': {
                 'hansen_loss_thresh': 'HANSEN/gfw_loss_by_year_threshold_2015',
                 'biomass_2000': 'users/davethau/whrc_carbon_test/carbon'
