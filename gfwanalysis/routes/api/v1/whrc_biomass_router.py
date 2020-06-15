fd64b29e907e8f6ff8a53ff8d99ac36d65269eff
Adam J. Pain <aj.pain89@gmail.com>
2020-06-15 12:14:26 +0200

    Set whrc density denominator to tc not area

--- gfwanalysis/routes/api/v1/whrc_biomass_router.py
+++ gfwanalysis/routes/api/v1/whrc_biomass_router.py
@@ -37,7 +37,7 @@ def analyze(geojson, area_ha):
         logging.error('[ROUTER]: ' + str(e))
         return error(status=500, detail='Generic Error')
     data['area_ha'] = area_ha
-    data['biomass_density'] = data['biomass'].get('b1') / area_ha
+    data['biomass_density'] = data['biomass'] / data['tree_cover'] if data['tree_cover'] > 0 else 0
     return jsonify(data=serialize_whrc_biomass(data, 'whrc-biomass')), 200
 
 
